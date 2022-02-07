2faa35adc4eb2fd856c64c8d721009b4387b45c9
husky <fekhesk@pm.me>
2022-02-07 13:06:44 -0800

    fix formatting

--- src/colours.f90
+++ src/colours.f90
@@ -8,23 +8,23 @@ module colours
 		character(len=3) :: c
 		select case (c)
 			case ('RED')
-				write(*, 'a', advance='no') achar(27) // '[31m'
+				write(*,'(a)',advance='no') achar(27) // '[31m'
 			case ('GRN')
-				write(*, 'a', advance='no') achar(27) // '[32m'
+				write(*,'(a)',advance='no') achar(27) // '[32m'
 			case ('YEL')
-				write(*, 'a', advance='no') achar(27) // '[33m'
+				write(*,'(a)',advance='no') achar(27) // '[33m'
 			case ('BLU')
-				write(*, 'a', advance='no') achar(27) // '[34m'
+				write(*,'(a)',advance='no') achar(27) // '[34m'
 			case ('PUR')
-				write(*, 'a', advance='no') achar(27) // '[35m'
+				write(*,'(a)',advance='no') achar(27) // '[35m'
 			case ('CYA')
-				write(*, 'a', advance='no') achar(27) // '[36m'
+				write(*,'(a)',advance='no') achar(27) // '[36m'
 			case ('BLK') !! BLK here means BLANK, aka default text
-				write(*, 'a', advance='no') achar(27) // '[0m'
+				write(*,'(a)',advance='no') achar(27) // '[0m'
 			case ('   ') !! three spaces to reset (:
-				write(*, 'a', advance='no') achar(27) // '[0m'
+				write(*,'(a)',advance='no') achar(27) // '[0m'
 			case default
-				write(*, 'a', advance='no') achar(27) // '[0m'
+				write(*,'(a)',advance='no') achar(27) // '[0m'
 		end select
 	end subroutine colour
 end module colours 
